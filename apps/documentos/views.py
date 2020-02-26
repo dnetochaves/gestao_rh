82435202f2c1cf916f2fa0939ba02c2181675095
dnetochaves <dnetochaves@gmail.com>
2020-02-26 12:50:13 -0300

    Finalizaçao Crud2 Departamento

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,26 @@
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Documento
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def Teste(request):
+    return render(request, 'documentos/teste.html')
+
+
+class DocumentoCreate(CreateView):
+    model = Documento
+    fields = ['descricao', 'arquivo']
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['funcionario_id']
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+ 
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +25,6 @@ urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('empresas/', include('apps.empresas.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
+    path('documentos/', include('apps.documentos.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
